256f97fc2638284348c14b883fb592b1a5a167a3
Derek In <53790662+derockenthis@users.noreply.github.com>
2021-07-23 10:28:44 -0700

    Add files via upload

--- api/api.py
+++ api/api.py
@@ -1,8 +1,14 @@
-import time
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if(request.method=="POST"):
+        print("HKFHJAKFJ")
+        name = (request.form.get("login"))
+        return {'name': name}
+    return "hello"
+
+if __name__ == "__main__":
+    app.run()
