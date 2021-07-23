eeaa4296419f937318b024c7c7faca5c70a238bc
Your Name <alassycottery@gmail.com>
2021-07-23 11:52:29 +0700

    can download file from database as .nc file

--- api/flaskapp/routes.py
+++ api/flaskapp/routes.py
@@ -1,8 +1,9 @@
-from flask import request, jsonify, abort, redirect, url_for, render_template
+from flask import request, jsonify, abort, redirect, url_for, render_template, send_file
 from flaskapp import app, db, bcrypt
 from flaskapp.models import UserModel, user_schema, users_schema, WorkflowModel
 from flask_login import login_user, current_user, logout_user
 import uuid
+from io import BytesIO
 
 # create calls for user database
 @app.route('/api/v1/register', methods=['POST'])
@@ -110,3 +111,9 @@ def publish():
         return file.filename + ' is saved.'
     if request.method == 'GET':
         return render_template('upload.html')
+
+
+@app.route('/api/v1/workflow/get/<file_id>', methods=['GET'])
+def get_workflow(file_id):
+    chosen_workflow = WorkflowModel.query.get(file_id)
+    return send_file(BytesIO(chosen_workflow.content), as_attachment=True, attachment_filename="download.nc")
