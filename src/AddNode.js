2c1e75bd9512d928bf1b0b1e3d443ffe131e7af7
Allyson <allrivera@csumb.edu>
2021-08-10 22:30:49 -0700

    updated git

---
+++ src/AddNode.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SaveRestore from './Save';
+import Save from './Save';
+
+
+export default () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+  return (
+    <aside>
+      <div className="description">You can drag these nodes to the pane.</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+        Input Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Default Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+        Output Node
+      </div>
+      <div className="dndnode text" onDragStart= {(event) => onDragStart(event, 'textNode')} draggable>
+        Text Node
+      </div>
+    </aside>
+  );
+};
