24db6a82e4ec9be4d24e34cd021f30f3dae7b53f
Allyson <allrivera@csumb.edu>
2021-08-15 20:37:02 -0700

    miniMap

---
+++ src/Pages/AddingNode.js
@@ -0,0 +1,37 @@
+import React, { memo } from 'react';
+
+import { Handle } from 'react-flow-renderer';
+
+
+export default memo(({ data, isConnectable }) => {
+  return (
+    <>
+      <Handle
+        type="target"
+        position="left"
+        style={{ background: '#555' }}
+        onConnect={(params) => console.log('handle onConnect', params)}
+        isConnectable={isConnectable}
+      />
+      <div className = "addNode">
+          
+        <input type="add" name="addText" placeholder="add"/>
+      </div>
+      
+      <Handle
+        type="source"
+        position="right"
+        id="a"
+        style={{ top: 10, background: '#555' }}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position="right"
+        id="b"
+        style={{ bottom: 10, top: 'auto', background: '#555' }}
+        isConnectable={isConnectable}
+      />
+    </>
+  );
+});
